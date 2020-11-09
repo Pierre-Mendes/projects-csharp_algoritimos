38d297ba41b24f622f1cf660b00897bb9dbc82ee
Pierre Mendes Salatiel <pierretielmendes@gmail.com>
2020-11-08 23:02:10 -0300

    Controle de Strings

---
+++ Strings Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Programas__Algoritmos_Estruturados_1per
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nome;
+            string Sobrenome;
+            string Apelido;
+
+            Nome =  " Meu nome é Pierre ";
+            Sobrenome = " Mendes Salatiel ";
+            Apelido = " Meu apelido é 3,14R ";
+            Console.WriteLine(Nome);
+             Console.WriteLine(Sobrenome);
+              Console.WriteLine(Apelido);
+        }
+    }
+}
