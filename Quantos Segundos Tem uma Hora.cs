8317e16e6de778677a9ccbd594d21277c1642adf
Pierre Mendes Salatiel <pierretielmendes@gmail.com>
2020-11-08 23:12:01 -0300

    Cálulo de quantos segundos tem uma hora

---
+++ Quantos Segundos Tem uma Hora.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		//Programa feito para desobrir quantos segundos existem em uma hora
+            int segundosEmUminuto = 60;
+            int segundosEmUmahora = 60;
+
+            Console.WriteLine(segundosEmUminuto *  segundosEmUmahora);
+        }
+    }
+}
